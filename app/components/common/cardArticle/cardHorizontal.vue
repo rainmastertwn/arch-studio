8edccc4726630857b4c50688af1d3998ba2036e4
Tim <tim.chao0825@gmail.com>
2025-11-28 17:35:25 +0800

    dev: ts型別調整

--- app/components/common/cardArticle/cardHorizontal.vue
+++ app/components/common/cardArticle/cardHorizontal.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-const { imgSrc, title, content, url, columns } = withDefaults(
+const props = withDefaults(
   defineProps<{
     imgSrc?: string
     title?: string
@@ -8,29 +8,32 @@ const { imgSrc, title, content, url, columns } = withDefaults(
     columns?: number[]
   }>(),
   {
-    columns: [5, 5]
+    columns: () => [5, 5]
   }
 )
 </script>
 
 <template>
-  <NuxtLink class="card-box" :to="url">
+  <NuxtLink class="card-box" :to="props.url">
     <div
       class="grid grid-cols-1 gap-4 md:gap-6 lg:gap-9"
-      :class="`lg:grid-cols-[${columns[0]}fr_${columns[1]}fr]`"
+      :class="{
+        'lg:grid-cols-[5fr_5fr]': props.columns[0] === 5 && props.columns[1] === 5,
+        'lg:grid-cols-[3fr_7fr]': props.columns[0] === 3 && props.columns[1] === 7
+      }"
     >
       <div>
         <div class="card-img overflow-hidden rounded-lg">
-          <img class="w-full" :src="imgSrc" />
+          <img class="w-full" :src="props.imgSrc" />
         </div>
       </div>
       <div class="card-content flex flex-col">
         <h4 class="mb-6 max-text-two-lines leading-8">
-          {{ title }}
+          {{ props.title }}
         </h4>
 
         <p class="text-sm text-gray-888888 text-justify max-text-three-lines leading-6">
-          {{ content }}
+          {{ props.content }}
         </p>
       </div>
     </div>
